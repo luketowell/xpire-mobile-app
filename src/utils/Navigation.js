7f4e2d2d48724cbf6343a81b8be6f896ec0ac9be
Luke Towell <luke.towell@hotmail.com>
2020-08-26 13:33:20 +0100

    removed all unnecessary imports

--- src/utils/Navigation.js
+++ src/utils/Navigation.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
 import 'react-native-gesture-handler';
-import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from '../screens/HomeScreen';
 import DetailsScreen from '../screens/DetailsScreen';
-import Header from '../components/Header';
-
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const Drawer = createDrawerNavigator();
 
