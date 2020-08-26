58d10bf834e0bb6b41531aab3c146925706a6796
Luke Towell <luke.towell@hotmail.com>
2020-08-26 19:38:59 +0100

    Applied Texts to the HomeScreen
    - Applied Texts to the HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 import Header from '../components/Header';
 import MainStyles from '../Assets/styles/MainStyles';
-import BigText from '../components/BigText';
+import { LargeText, SmallText, MediumText } from '../components/Text';
 
 function HomeScreen({ navigation }) {
     return (
@@ -10,10 +10,28 @@ function HomeScreen({ navigation }) {
             <SafeAreaView style={MainStyles.top} />
             <SafeAreaView style={MainStyles.container}>
                 <Header navigation={navigation} />
-                <View style={{ flex: 0.3, backgroundColor: 'blue' }}>
-                    <BigText text="Welcome to Xpire!" />
+                <View
+                    style={{
+                        flex: 0.3,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}>
+                    <LargeText>
+                        <Text>Welcome to Xpire!</Text>
+                    </LargeText>
+                    <SmallText>
+                        <Text>The waste reduction application.</Text>
+                    </SmallText>
+                    <View style={{ marginTop: 40 }}>
+                        <MediumText>
+                            <Text>
+                                Please choose the department {'\n'} which you
+                                would like to check today
+                            </Text>
+                        </MediumText>
+                    </View>
                 </View>
-                <View style={{ flex: 0.6, backgroundColor: 'red' }}></View>
+                <View style={{ flex: 0.6 }}></View>
             </SafeAreaView>
         </Fragment>
     );
