ddbe99deab2244356618f64ed4cdad095bd4e49b
Luke Towell <luke.towell@hotmail.com>
2020-08-29 13:46:00 +0100

    Updated the text field to take props on alignment
    - updated the Medium Text to take props on alignment within the app

--- src/components/Text.js
+++ src/components/Text.js
@@ -6,8 +6,12 @@ export function SmallText({ children }) {
     return <Text style={TextStyles.small}>{children}</Text>;
 }
 
-export function MediumText({ children }) {
-    return <Text style={TextStyles.medium}>{children}</Text>;
+export function MediumText({ children, align }) {
+    return (
+        <Text style={[TextStyles.medium, align ? { textAlign: align } : null]}>
+            {children}
+        </Text>
+    );
 }
 export function LargeText({ children }) {
     return <Text style={TextStyles.large}>{children}</Text>;
