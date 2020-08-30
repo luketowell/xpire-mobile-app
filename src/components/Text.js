b813d2645bacdc77e11fa88c4261a7163b3608c0
Luke Towell <luke.towell@hotmail.com>
2020-08-30 10:35:01 +0100

    Updated text component to take colour prop
    - updated all text components to ensure that they can take colour props

--- src/components/Text.js
+++ src/components/Text.js
@@ -2,21 +2,40 @@ import React from 'react';
 import { Text } from 'react-native';
 import TextStyles from '../Assets/styles/components/TextStyles';
 
-export function SmallText({ children, align }) {
+export function SmallText({ children, align, color }) {
     return (
-        <Text style={[TextStyles.small, align ? { textAlign: align } : null]}>
+        <Text
+            style={[
+                TextStyles.small,
+                align ? { textAlign: align } : null,
+                color ? { color: color } : null,
+            ]}>
             {children}
         </Text>
     );
 }
 
-export function MediumText({ children, align }) {
+export function MediumText({ children, align, color }) {
     return (
-        <Text style={[TextStyles.medium, align ? { textAlign: align } : null]}>
+        <Text
+            style={[
+                TextStyles.medium,
+                align ? { textAlign: align } : null,
+                color ? { color: color } : null,
+            ]}>
             {children}
         </Text>
     );
 }
-export function LargeText({ children }) {
-    return <Text style={TextStyles.large}>{children}</Text>;
+export function LargeText({ children, color, align }) {
+    return (
+        <Text
+            style={[
+                TextStyles.large,
+                align ? { textAlign: align } : null,
+                color ? { color: color } : null,
+            ]}>
+            {children}
+        </Text>
+    );
 }
