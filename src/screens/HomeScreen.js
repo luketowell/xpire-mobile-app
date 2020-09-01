3aa2831970082bd94dc76593818e1b935c6c5496
Luke Towell <luke.towell@hotmail.com>
2020-09-02 00:07:36 +0100

    Added ability to scan from homepage
    - added functionality to scan from the homepage

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, Text, View, TouchableOpacity } from 'react-native';
 import Header from '../components/Header';
 import MainStyles from '../Assets/styles/MainStyles';
 import { LargeText, SmallText, MediumText } from '../components/Text';
 import HomeStyles from '../Assets/styles/pages/HomeStyles';
 import CategoryList from '../components/CategoryList';
+import { primaryGreen } from '../Assets/styles/variables/variables';
 
 function HomeScreen({ navigation }) {
     return (
@@ -19,13 +20,35 @@ function HomeScreen({ navigation }) {
                     <SmallText>
                         <Text>The waste reduction application.</Text>
                     </SmallText>
-                    <View style={{ marginTop: 40 }}>
+                    <View style={{ marginTop: 20 }}>
                         <MediumText>
                             <Text>
-                                Please choose the department {'\n'} which you
-                                would like to check today
+                                How would you like to check stock expiry:
                             </Text>
                         </MediumText>
+                    </View>
+                    <View style={{ paddingBottom: 20 }}>
+                        <MediumText align={'left'}>By Barcode:</MediumText>
+                        <TouchableOpacity
+                            onPress={() => {
+                                navigation.navigate('Scanner');
+                            }}
+                            style={{
+                                marginTop: 10,
+                                backgroundColor: primaryGreen,
+                                alignSelf: 'center',
+                                paddingHorizontal: 40,
+                                paddingVertical: 15,
+                                borderWidth: 2,
+                                borderColor: 'black',
+                                borderRadius: 6,
+                                flexDirection: 'row',
+                            }}>
+                            <Text>Scan</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <MediumText align={'left'}>By Category:</MediumText>
                     </View>
                 </View>
                 <View style={{ flex: 0.6 }}>
