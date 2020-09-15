b52f9508be0913073e2949af8ddbf0d2e69a4563
Luke Towell <luke.towell@hotmail.com>
2020-09-15 16:03:29 +0100

    updated the storeItem model
    - updated the storeItem model to match the Db reference

--- src/models/StoreItem.js
+++ src/models/StoreItem.js
@@ -1,7 +1,7 @@
 class StoreItem {
     constructor(barcode, expiry_date, storeId, updated_date) {
         (this.item_upc = parseInt(barcode)),
-            (this.store_id = storeId),
+            (this.storeId = storeId),
             (this.expiry_date = expiry_date.toUTCString()),
             (this.created_date = new Date().toUTCString()),
             (this.updated_date = updated_date);
